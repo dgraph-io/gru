1fabba535b80c34085aef6b6bbb1663291bd1a21
Pawan Rawal <pawan@dgraph.io>
2018-04-23 16:51:28 +1000

    Fix old backup file deletion logic.

--- x/x.go
+++ x/x.go
@@ -7,7 +7,7 @@ import (
 	"math"
 	"net/http"
 	"os"
-	"strings"
+	"path/filepath"
 	"time"
 
 	"github.com/dgraph-io/gru/admin/company"
@@ -15,7 +15,10 @@ import (
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
-var debug = flag.Bool("debug", false, "Whether to print debug info")
+var (
+	debug  = flag.Bool("debug", false, "Whether to print debug info")
+	backup = flag.String("backup", "", "Dgraph backup directory path")
+)
 
 func Debug(log interface{}) {
 	if *debug {
@@ -86,17 +89,18 @@ func Backup() {
 var layout = "2006-01-02-15-04"
 
 func check(file os.FileInfo) {
-	// Filename is like dgraph-0-2016-12-15-20-12.rdf.gz
-	s := strings.Split(file.Name(), "-")
-	if len(s) != 7 {
-		fmt.Println("Can't parse file name format.")
+	fname := file.Name()
+	// Filename is like dgraph-0-2016-12-15-20-12.rdf.gz or dgraph-schema-1-2016-12-15-20-12.rdf.gz
+	// Length of file name should be atleast 16(for datetime) + 7(.rdf.gz)
+	if len(fname) < 23 {
+		fmt.Printf("Can't parse file name format: %+v\n", fname)
 		return
 	}
-	dateTime := s[2:6]
-	// Last string is like 12.rdf.gz
-	minutes := s[6][:2]
-	dateTime = append(dateTime, minutes)
-	t, err := time.Parse(layout, strings.Join(dateTime, "-"))
+	// remove .rdf.gz
+	fname = fname[:len(fname)-7]
+	dateTime := fname[len(fname)-16:]
+
+	t, err := time.Parse(layout, dateTime)
 	if err != nil {
 		fmt.Println("While parsing backup filename: ", file.Name())
 		return
@@ -109,22 +113,28 @@ func check(file os.FileInfo) {
 	}
 
 	if time.Now().After(t.Add(time.Duration(c.BackupDays) * 24 * time.Hour)) {
-		if err := os.Remove(fmt.Sprintf("backup/%v", file.Name())); err != nil {
+		if err := os.Remove(filepath.Join(*backup, file.Name())); err != nil {
 			fmt.Println("While removing file with name: ", err)
 		}
+		fmt.Println("Deleted old backup file: ", fname)
+	}
+}
+
+func deleteOldBackups() {
+	files, err := ioutil.ReadDir(*backup)
+	if err != nil {
+		fmt.Println("While reading backup directory: ", err)
+		return
+	}
+	for _, file := range files {
+		check(file)
 	}
 }
 
 func DeleteOldBackups() {
 	ticker := time.NewTicker(24 * time.Hour)
+	deleteOldBackups()
 	for range ticker.C {
-		files, err := ioutil.ReadDir("backup")
-		if err != nil {
-			fmt.Println("While reading backup directory: ", err)
-			continue
-		}
-		for _, file := range files {
-			check(file)
-		}
+		deleteOldBackups()
 	}
 }
